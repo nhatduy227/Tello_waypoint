217ec969d9e8804a516a72fbac6cfb9f254c4f3c
Nom Phan <np656@drexel.edu>
2020-05-30 17:30:17 -0400

    Tello_go_navigation added

--- Python scripts/Return_to_home.py
+++ Python scripts/Return_to_home.py
@@ -3,12 +3,12 @@ from easytello import tello
 t = tello.Tello()
 
 t.takeoff()
-attitude_list = []
-for i in range(3):
-    t.forward(100)
-    attitude = t.get_attitude()
-    t.cw(90)
-    attitude_list.append(attitude)
-first_point = attitude_list[0]
-t.go(first_point[0],first_point[1],first_point[2],30)
+# attitude_list = []
+# for i in range(3):
+#     t.forward(100)
+#     attitude = t.get_attitude()
+#     t.cw(90)
+#     attitude_list.append(attitude)
+# first_point = attitude_list[0]
+# t.go(first_point[0],first_point[1],first_point[2],30)
 t.land()
